a19ce1fafc512141601131871aba1ad0e5ced36b
Pietreus <peter.blohm@outlook.at>
2019-03-02 22:24:47 +0100

    Change variable names and refine code
    small changes in the script so make the script as well as
    the dataset more readable

--- run_analysis.R
+++ run_analysis.R
@@ -7,47 +7,39 @@
 #       for each activity and each subject.
 
 #Note: "UCI HAR Dataset" has to be in working dir
-library(dplyr)
+require(dplyr)
+#reading data from various files
+traindata <- read.table(file.path("UCI HAR Dataset","train","X_train.txt"))
+trainAct <- read.table(file.path("UCI HAR Dataset","train","y_train.txt"))
+trainSub <- read.table(file.path("UCI HAR Dataset","train","subject_train.txt"))
 
+testdata <- read.table(file.path("UCI HAR Dataset","test","X_test.txt"))
+testAct <- read.table(file.path("UCI HAR Dataset","test","y_test.txt"))
+testSub <- read.table(file.path("UCI HAR Dataset","test","subject_test.txt"))
 
-#TODO filepath variables are useless, combine statements
-trainPath <- file.path("UCI HAR Dataset","train","X_train.txt")
-trainAct <- file.path("UCI HAR Dataset","train","y_train.txt")
-trainSub <- file.path("UCI HAR Dataset","train","subject_train.txt")
-
-testPath <- file.path("UCI HAR Dataset","test","X_test.txt")
-testAct <- file.path("UCI HAR Dataset","test","y_test.txt")
-testSub <- file.path("UCI HAR Dataset","test","subject_test.txt")
-
-featurePath <- file.path("UCI HAR Dataset","features.txt")
-
-
-traindata <- read.table(trainPath)
-testdata <- read.table(testPath)
-features <- read.table(featurePath)[,2]
 
 fulldata <- rbind(traindata,testdata)
-activities <- rbind(read.table(trainAct),read.table(testAct))
+activities <- rbind(trainAct, testAct)
 activitylabels <- read.table(file.path("UCI HAR Dataset","activity_labels.txt"))
-actLabeled <- merge(activities,activitylabels)[,2]
-subjects <- rbind(read.table(trainSub),read.table(testSub))
-names(fulldata) <- features
+actLabeled <- merge(activities, activitylabels)[,2]
+subjects <- rbind(trainSub, testSub)
+#reading the original variable names
+names(fulldata) <- read.table(file.path("UCI HAR Dataset","features.txt"))[,2]
 
-#extracting only mean() and std() variables
+#extracting only mean() and std() variables and adapting variable names
 
-impNames <- grep("-mean\\(\\)|-std\\(\\)",features)
-
+impNames <- grep("-mean\\(\\)|-std\\(\\)", names(fulldata))
 dataTbl <- tbl_df(fulldata[,impNames])
-#Table required in Step 4 of the assignment
+names(dataTbl) <- tolower(gsub("\\(\\)","",names(dataTbl)))
+
+#the Table required in Step 4 of the assignment
 fullTbl <- mutate(dataTbl, Activity = actLabeled)
 
 
-#TODO
-#Variable names may be too ambiguous -- fix with regex
-#create the transformed dataset with the mean for each sub/act/var
-#for loops for subjects and activities -> calc mean for each variable
-#better: group and take mean per group
-
+#adding the subject column, grouping by subject/activity
+#and take the mean for each column per group
+#the tidy dataset, as requested in the assignment
+#i love dplyr
 transformedTbl <- fullTbl %>% mutate( subjectID = subjects[,1]) %>%
                               group_by(subjectID, Activity) %>%
                               summarise_all(list(mean)) %>%
